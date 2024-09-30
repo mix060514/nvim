64f090f94db7d9b4f06f9b81b716b8fc345c2abe
ivan <ivan.zhang@acer.com>
2024-09-30 09:34:02 +0800

    check if dir is exist

--- init.lua
+++ init.lua
@@ -8,7 +8,23 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Reload vimrc
-vim.keymap.set("n", "<leader>rc", ":source c:/Users/TheWorld/AppData/Local/nvim/init.lua<CR>", opts)
+-- use if else to avoid conflict with different devices
+function is_folder_exist(folder_path)
+    local success, message = pcall(function()
+        local handle = io.popen("cd " .. folder_path)
+        local result = handle:read("*a")
+        handle:close()
+        return result
+    end)
+    return success
+end
+
+local folder_path = "c:/Users/TheWorld/AppData/Local/nvim"
+if is_folder_exist(folder_path) then
+    vim.keymap.set("n", "<leader>rc", ":source c:/Users/TheWorld/AppData/Local/nvim/init.lua<CR>", opts)
+else
+    vim.keymap.set("n", "<leader>rc", ":source c:/Users/2012044/AppData/Local/nvim/init.lua<CR><CR>", opts)
+end
 
 require("config.lazy")
 require("user.vscode_keymaps")
